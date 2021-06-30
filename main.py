e7c14d850a1fb0d4daff97c5de652b2d86c76f71
pixelsuft <yuskivl07@mail.ru>
2021-06-30 17:12:33 +0700

    own support

--- main.py
+++ main.py
@@ -10,37 +10,59 @@ rom = list(js.window.rom_content)
 
 worlds_offset = 7364
 levels_offset = worlds_offset + 8
-
-use_random = js.confirm('Generate?')
-#if not use_random:
-#    js.location.href = 'emulator.html'
-
-generate_world = js.confirm('Generate worlds - OK, Generate Levels - Cancel')
 title = ''
 
-if generate_world:
-    rom[worlds_offset] = generate()
-    rom[worlds_offset + 1] = generate()
-    rom[worlds_offset + 2] = generate()
-    rom[worlds_offset + 3] = generate()
-    rom[worlds_offset + 4] = generate()
-    rom[worlds_offset + 5] = generate()
-    rom[worlds_offset + 6] = generate()
-    rom[worlds_offset + 7] = generate()
-    title = ','.join(map(str, rom[worlds_offset:worlds_offset + 7]))
-    js.alert(title + '\nRemember it!')
+use_gen = js.confirm('Generate - OK, Use own - Cancel')
+if use_gen:
+    generate_world = js.confirm('Generate worlds - OK, Generate Levels - Cancel')
+    if generate_world:
+        rom[worlds_offset] = generate()
+        rom[worlds_offset + 1] = generate()
+        rom[worlds_offset + 2] = generate()
+        rom[worlds_offset + 3] = generate()
+        rom[worlds_offset + 4] = generate()
+        rom[worlds_offset + 5] = generate()
+        rom[worlds_offset + 6] = generate()
+        rom[worlds_offset + 7] = generate()
+        title = ','.join(map(str, rom[worlds_offset:worlds_offset + 8]))
+        js.alert(title + '\nRemember it!')
+    else:
+        rom[levels_offset] = generate()
+        rom[levels_offset + 1] = generate()
+        rom[levels_offset + 2] = generate()
+        rom[levels_offset + 3] = generate()
+        rom[levels_offset + 4] = generate()
+        rom[levels_offset + 5] = generate()
+        rom[levels_offset + 6] = generate()
+        rom[levels_offset + 7] = generate()
+        rom[levels_offset + 8] = generate()
+        title = ','.join(map(str, rom[levels_offset:levels_offset + 9]))
+        js.alert(title + '\nRemember it!')
 else:
-    rom[levels_offset] = generate()
-    rom[levels_offset + 1] = generate()
-    rom[levels_offset + 2] = generate()
-    rom[levels_offset + 3] = generate()
-    rom[levels_offset + 4] = generate()
-    rom[levels_offset + 5] = generate()
-    rom[levels_offset + 6] = generate()
-    rom[levels_offset + 7] = generate()
-    rom[levels_offset + 8] = generate()
-    title = ','.join(map(str, rom[levels_offset:levels_offset + 7]))
-    js.alert(title + '\nRemember it!')
+    generate_world = js.confirm('Use worlds - OK, Use Levels - Cancel')
+    inserted = list(map(int, js.prompt('Enter code: ').split(',')))
+    print(inserted)
+    if generate_world:
+        rom[worlds_offset] = inserted[0]
+        rom[worlds_offset + 1] = inserted[1]
+        rom[worlds_offset + 2] = inserted[2]
+        rom[worlds_offset + 3] = inserted[3]
+        rom[worlds_offset + 4] = inserted[4]
+        rom[worlds_offset + 5] = inserted[5]
+        rom[worlds_offset + 6] = inserted[6]
+        rom[worlds_offset + 7] = inserted[7]
+        title = ','.join(map(str, rom[worlds_offset:worlds_offset + 8]))
+    else:
+        rom[levels_offset] = inserted[0]
+        rom[levels_offset + 1] = inserted[1]
+        rom[levels_offset + 2] = inserted[2]
+        rom[levels_offset + 3] = inserted[3]
+        rom[levels_offset + 4] = inserted[4]
+        rom[levels_offset + 5] = inserted[5]
+        rom[levels_offset + 6] = inserted[6]
+        rom[levels_offset + 7] = inserted[7]
+        rom[levels_offset + 8] = inserted[8]
+        title = ','.join(map(str, rom[levels_offset:levels_offset + 9]))
 
 
 converted = ','.join(map(str, rom))
